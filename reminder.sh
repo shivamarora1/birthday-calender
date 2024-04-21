a00c4edd8703e0b991298f721a3e5edf4a7af4b5
Shivam Arora <shivamarora1@yahoo.com>
2024-04-21 16:48:15 +0530

    added reminder sh file

--- reminder.sh
+++ reminder.sh
@@ -21,3 +21,6 @@ if [[ $(echo "$api_result" | jq '.[]') ]]; then
 fi
 
 echo "API call successful!"            
+
+# This script will raise error if birthday api returns entry.
+# On error gitaction workflow will send mail and this will act like a email reminder.
